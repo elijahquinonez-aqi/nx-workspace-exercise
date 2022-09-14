a7d02cc2e068a954e0e8e9479f278ab2d4e58d33
elijahquinonez <elijahquinonez@sedaru.com>
2022-09-13 17:36:49 -0700

    libs

--- myorg/apps/api/src/app/app.service.ts
+++ myorg/apps/api/src/app/app.service.ts
@@ -1,8 +1,14 @@
+import { Todo } from '@myorg/data';
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getData(): { message: string } {
-    return { message: 'Welcome to api!' };
+  todos: Todo[] = [{ title: 'Todo 1' }, { title: 'Todo 2' }];
+  getData(): Todo[] {
+    return this.todos;
+  }
+
+  addTodo() {
+    this.todos.push({ title: `New todo ${Math.floor(Math.random() * 1000)}` });
   }
 }
--- myorg/apps/todos/src/app/app.component.ts
+++ myorg/apps/todos/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Todo } from '@myorg/data';
 
 @Component({
   selector: 'myorg-root',
@@ -8,23 +9,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todos';
-  todos: Todo[] = [{ title: 'Todo 1' }, { title: 'Todo 2' }];
+  todos: Todo[] = [];
 
   constructor(private http: HttpClient) {
     this.fetch();
   }
 
   addTodo() {
-    this.todos.push({
-      title: `New todo ${Math.floor(Math.random() * 1000)}`,
+    this.http.post('/api/addTodo', {}).subscribe(() => {
+      this.fetch();
     });
   }
 
   fetch() {
     this.http.get<Todo[]>('/api/todos').subscribe((t) => (this.todos = t));
   }
-}
-
-interface Todo {
-  title: string;
 }
